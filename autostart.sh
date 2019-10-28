3be8d6418b6d8a23b29440d09c91b8e527f8267b
David Chen <weichen2000121@gmail.com>
2019-10-28 14:50:50 -0700

    name change

--- autostart.sh
+++ autostart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 /bin/bash ~/scripts/dwm-status.sh &
-/bin/bash ~/scripts/wp-set.sh &
+/bin/bash ~/scripts/wp-autochange.sh &
 picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 --backend glx -b
 /bin/bash ~/scripts/tap-to-click.sh &
 /bin/bash ~/scripts/inverse-scroll.sh &
