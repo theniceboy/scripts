c8ec0042c4d06eb6caa6aaf4f830818157fad513
David Chen <weichen2000121@gmail.com>
2019-11-01 08:37:06 -0700

    update status

--- dwm-status.sh
+++ dwm-status.sh
@@ -112,7 +112,7 @@ do
 	vel_recv=$(get_velocity $received_bytes $old_received_bytes $now)
 	vel_trans=$(get_velocity $transmitted_bytes $old_transmitted_bytes $now)
 
-	xsetroot -name "💿$(print_mem)M ⬇️$vel_recv ⬆️$vel_trans $(dwm_alsa) BAT: $(print_bat)$(show_record) $(print_date)"
+	xsetroot -name "💿$(print_mem)M ⬇️$vel_recv ⬆️$vel_trans $(dwm_alsa) BAT: [ $(print_bat) ]$(show_record) $(print_date)"
 
 	# Update old values to perform new calculations
 	old_received_bytes=$received_bytes
