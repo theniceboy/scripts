35e2b7a605ded8c403c6c33af374969cebe73cd5
David Chen <weichen2000121@gmail.com>
2020-01-28 12:10:07 -0800

    remove trayer command

--- tr-toggle.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-result=$(ps ax|grep -v grep|grep trayer)
-if [ "$result" == "" ]; then
-  eval  "trayer --transparent true --expand false --align right --width 20 --SetDockType false --tint 0x88888888 &"
-else
-  eval "killall trayer"
-fi
