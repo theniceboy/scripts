a24e8090df5020768ce9a863ff0f71f80fdeaa64
David Chen <weichen2000121@gmail.com>
2020-02-21 11:37:58 -0800

    2mon setup

--- autostart.sh
+++ autostart.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-xrandr --auto --output HDMI1 --same-as eDP1 --size 1920x1080
+xrandr --auto --output DP-2 --same-as eDP-1 --size 1920x1080
+xrandr --auto --output HDMI-1  --right-of DP-2 --size 1920x1080
 /bin/bash ~/scripts/dwm-status.sh &
 /bin/bash ~/scripts/wp-autochange.sh &
 picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
