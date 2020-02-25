db6f9061dbee0044f6ed198fb5cb3679952d512a
David Chen <weichen2000121@gmail.com>
2020-02-25 16:11:38 +0000

    Revert "auto mon"
    This reverts commit 0edc62dfa961c0a140683cec644ac1c0b37f5f00.

--- autostart.sh
+++ autostart.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-xrandr --auto --output HDMI1 --same-as eDP1 --size 1920x1080
 /bin/bash ~/scripts/dwm-status.sh &
 /bin/bash ~/scripts/wp-autochange.sh &
 picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
