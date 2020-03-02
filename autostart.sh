a7aaf1c06672df209ed8e1b576c7840f8adec5ef
David Chen <weichen2000121@gmail.com>
2020-03-02 11:07:19 -0800

    autostart, fzf_panes

--- autostart.sh
+++ autostart.sh
@@ -2,12 +2,13 @@
 
 /bin/bash ~/scripts/dwm-status.sh &
 /bin/bash ~/scripts/wp-autochange.sh &
-picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
+#picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
+picom -b
 /bin/bash ~/scripts/tap-to-click.sh &
 /bin/bash ~/scripts/inverse-scroll.sh &
 /bin/bash ~/scripts/setxmodmap-colemak.sh &
 nm-applet &
 xfce4-power-manager &
-xfce4-volumed-pulse &
+#xfce4-volumed-pulse &
 /bin/bash ~/scripts/run-mailsync.sh &
 ~/scripts/autostart_wait.sh &
