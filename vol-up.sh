6be0a61e35899327a616d7a57c83c8be86a84846
David Chen <weichen2000121@gmail.com>
2020-03-06 11:23:16 -0800

    vol control

--- vol-up.sh
+++ vol-up.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-#/usr/bin/amixer -qM set Master 5%+ umute
-pactl set-sink-volume @DEFAULT_SINK@ +5%
+/usr/bin/amixer -qM set Master 5%+ umute
+#pactl set-sink-volume @DEFAULT_SINK@ +5%
 bash ~/scripts/dwm-status-refresh.sh
