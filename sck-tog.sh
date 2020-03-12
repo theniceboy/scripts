656a60c8259936acbc88570514d6a0873456e0fc
David Chen <weichen2000121@gmail.com>
2020-03-11 22:38:08 -0700

    screenkey command update

--- sck-tog.sh
+++ sck-tog.sh
@@ -2,7 +2,7 @@
 
 result=$(ps ax|grep -v grep|grep screenkey)
 if [ "$result" == "" ]; then
-  eval "screenkey --bg-color white --font-color black &"
+  eval "screenkey --bg-color white --font-color black --font SauceCodePro --opacity 0.9 -t 2 &"
 else
   eval "killall screenkey"
 fi
