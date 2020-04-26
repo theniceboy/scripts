404b2cf27d22b12f70d7531f50fffdb777a726b5
David Chen <weichen2000121@gmail.com>
2020-04-26 12:07:06 -0400

    brew stuff

--- install-brew-packages.sh
+++ install-brew-packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-brew install neofetch jesseduffield/lazygit/lazygit git-delta the_silver_searcher ccat ripgrep shellcheck
-yarn global add diagnostic-languageserver
-flutter pub global activate dart_style
+brew install neofetch jesseduffield/lazygit/lazygit git-delta the_silver_searcher ccat ripgrep fzf ncdu tldr shellcheck
+#yarn global add diagnostic-languageserver
+#flutter pub global activate dart_style
 
