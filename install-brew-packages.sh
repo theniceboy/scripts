0f633549c1e6709be2be69a94382842bf7d47b87
David Chen <chenwei2000121@163.com>
2020-05-07 16:42:19 -0400

    rainbarf

--- install-brew-packages.sh
+++ install-brew-packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-brew install neofetch jesseduffield/lazygit/lazygit git-delta the_silver_searcher ccat ripgrep fzf ncdu tldr neovim fd yarn shellcheck loc
+brew install neofetch jesseduffield/lazygit/lazygit git-delta the_silver_searcher ccat ripgrep fzf ncdu tldr neovim fd yarn shellcheck loc rainbarf
 sudo chgrp -R sudo $(brew --prefix)/*; sudo chmod -R g+w $(brew --prefix)/*
 yarn global add diagnostic-languageserver
 #flutter pub global activate dart_style
